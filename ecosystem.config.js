faeef207f249f664c085a421a3ccad0a93239a44
Billing System <billing@system.local>
2026-01-27 15:25:56 +0700

    Config: Change port back to 3001

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,11 @@ module.exports = {
     exp_backoff_restart_delay: 100,
     env: {
       NODE_ENV: "production",
-      PORT: 3011,
+      PORT: 3001,
       PUPPETEER_EXECUTABLE_PATH: "/home/adi/.cache/puppeteer/chrome/linux-144.0.7559.96/chrome-linux64/chrome"
     },
 
-    PORT: 3011,
+    PORT: 3001,
     error_file: "./logs/pm2-error.log",
     out_file: "./logs/pm2-out.log"
   }]
